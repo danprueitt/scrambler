2b48dfa8a0262b89d21388f60c8abae78422cc65
Daniel Prueitt <danielprueitt@gmail.com>
2025-03-06 15:23:10 +0900

    feat: add vercel analytics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import './App.css'
-import Form from '../Form/Form'
+import React from "react";
+import "./App.css";
+import Form from "../Form/Form";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <h3>Another Scrambler</h3>
-            </header>
-            <p className='App-intro'>Enter a sentence on each line to be scrambled.</p>
-            <Form></Form>
-        </div>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Another Scrambler</h3>
+      </header>
+      <p className="App-intro">
+        Enter a sentence on each line to be scrambled.
+      </p>
+      <Form></Form>
+      <Analytics />
+    </div>
+  );
 }
 
-export default App
+export default App;
